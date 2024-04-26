38bb7bee34eecfb87badf2884cd422374bc20873
oli <oli@hlmpn.dev>
2024-04-26 12:15:33 +0200

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gopkg.hlmpn.dev/uuid
+
+go 1.22.2
---
+++ uuid.go
@@ -0,0 +1,23 @@
+package uuid
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+)
+
+type UUID string
+
+var err error
+
+func New() (string, error) {
+	b := make([]byte, 16)
+	_, err = rand.Read(b)
+	if err != nil {
+		return "", err
+	}
+	uuid := hex.EncodeToString(b)
+	// Insert hyphens after every 8 characters
+	uuidWithHyphens := uuid[0:8] + "-" + uuid[8:16] + "-" + uuid[16:24] + "-" + uuid[24:]
+
+	return string(uuidWithHyphens), nil
+}
